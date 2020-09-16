40cf313ad6aa9b384a0d964c8d0a99efeba60998
OwenRamire <owenomar_ramirez@ucol.mx>
2020-09-15 19:04:20 -0500

    Create new constant message

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,2 +1,10 @@
 export const NEXT_MESSAGE = 'Siguiente';
 export const ACCEPT = 'Aceptar';
+export const IS_REQUIRED = 'Requerido';
+export const PASS_REQUIREMENTS = {
+  firstTerm: 'tener 8 o más caracteres',
+  secondTerm: 'contener por lo menos 1 letra minúscula',
+  thirdTerm: 'contener por lo menos 1 letra mayúscula',
+  fourthTerm: 'contener por lo menos 1 dígito',
+};
+export const REREQUEST_PASS = 'Escribe de nuevo la clave';
