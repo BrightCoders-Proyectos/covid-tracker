c542c1b77ff67ab30b963c5fabb9e0b1ec426a66
OwenRamire <owenomar_ramirez@ucol.mx>
2020-10-08 17:57:43 -0500

    Add new constants messages

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -11,4 +11,6 @@ export const REREQUEST_PASS = 'Escribe de nuevo la clave';
 export const CONTACT_INFO = 'Información de Contacto';
 export const DESC_CONTACT_INFO =
   'Esta información servirá para ponernos en contacto contigo más rapido cuando sea necesario';
-export const PREFERABLY_CELLPHONE = ' De preferencia un número de celular';
+export const PREFERABLY_CELLPHONE = 'De preferencia un número de celular';
+export const DONT_ACCOUNT = '¿No tienes cuenta? Regístrate';
+export const FORGOT_PASSWORD = '¿Olvidaste tu clave?';
