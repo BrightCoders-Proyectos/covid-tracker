ca179c637ff62657568d958a3e6603ffedba687f
OwenRamire <owenomar_ramirez@ucol.mx>
2020-10-16 17:09:05 -0500

    Import EmailPassTxt and DescResetPass components

--- src/screens/signInScreens/ForgotYourPassword.js
+++ src/screens/signInScreens/ForgotYourPassword.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
-import {RESET_PASSWORD, RESET_PASS_INSTRUCTIONS} from '../../utils/Constants';
-import {TITLE_SIZE, INSTRUCTION_TXT_SIZE} from '../../utils/StylesConstants';
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import {Colors} from '../../utils/Colors';
+import {RESET_PASSWORD} from '../../utils/Constants';
+import {TITLE_SIZE} from '../../utils/StylesConstants';
+import DescResetPass from '../../components/DescResetPass';
+import EmailPassTxtInput from '../../components/EmailPassTxtInput';
 
 const ForgotYourPassword = () => {
   return (
     <SafeAreaView style={styles.mainContainer}>
-      <Text style={styles.title}>{RESET_PASSWORD}</Text>
-      <View>
-        <Text style={styles.txtInstructions}>{RESET_PASS_INSTRUCTIONS}</Text>
-      </View>
+      <ScrollView>
+        <Text style={styles.title}>{RESET_PASSWORD}</Text>
+        <DescResetPass />
+        <EmailPassTxtInput
+          title="Correo electrónico"
+          keyboard="email-address"
+          password={false}
+        />
+        <TouchableOpacity
+          style={styles.resetPassBtn}
+          onPress={() => {
+            console.log('Hello world');
+          }}>
+          <Text style={styles.resetPassTxt}>{RESET_PASSWORD}</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -22,12 +43,21 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: TITLE_SIZE,
-    marginVertical: 25,
+    marginTop: 25,
     textAlign: 'center',
   },
-  txtInstructions: {
-    fontSize: INSTRUCTION_TXT_SIZE,
-    textAlign: 'justify',
-    paddingHorizontal: 38,
+  resetPassBtn: {
+    height: 55,
+    marginVertical: 50,
+    marginHorizontal: 30,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.btnsColor,
+    borderRadius: 60,
+  },
+  resetPassTxt: {
+    color: Colors.white,
+    fontSize: 22,
   },
 });
